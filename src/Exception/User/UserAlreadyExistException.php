29a39d9b94f828bae35dd3fd04dc3b666c96a9fc
Juan <juan.gonzalez@trivago.com>
2019-12-26 15:46:29 +0100

    Update ExpensePreWriteListener, exceptions and test

--- src/Exception/User/UserAlreadyExistException.php
+++ src/Exception/User/UserAlreadyExistException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Exception\User;
 
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
-class UserAlreadyExistException extends BadRequestHttpException
+class UserAlreadyExistException extends ConflictHttpException
 {
     private const MESSAGE = 'User with email %s already exist';
 
